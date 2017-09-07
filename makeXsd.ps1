b9339bf29fcbb339ae7858e5d1615ca54892c1ee
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-09-07 23:38:40 +0200

    Make xsd package test

---
+++ makeXsd.ps1
@@ -0,0 +1,4 @@
+﻿ $version = "4.5.0-beta3"
+ 
+ 
+ msbuild  src/NLog.proj /verbosity:m  /t:xsd /t:NuGetSchemaPackage /p:Configuration=Release /p:BuildNetFX45=true /p:BuildVersion=$version   /p:configuration=release /p:BuildLabelOverride=NONE 
